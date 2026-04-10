502e7caa7912790247e7e681613589a3aecf653d
abenez16 <abenez16@gmail.com>
2026-04-09 20:59:10 -0500

    BankService

--- Service/BankService.java
+++ Service/BankService.java
@@ -1,1 +1,30 @@
+package service;
+
+import model.Account;
+import model.Customer;
+import model.Admin;
+import exceptions.AccountNotFoundException;
+import exceptions.InvalidAmountException;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+public class BankService {
+    private HashMap<String, Customer> customers;
+    private HashMap<String, Account> accounts;
+    private int customerCounter;
+    private int accountCounter;
+
+
+    public BankService() {
+        this.customers = new HashMap<>();
+        this.accounts = new HashMap<>();
+        this.customerCounter = 1000;
+        this.accountCounter = 1000;
+    }
+    public Customer createCustomer(String name, String password) {
+        String customerId = "CUS-" + customerCounter;
+        customerCounter++;
+
+        Customer newCustomer = new Customer(customerId, name, password);
+        customers.put(customerId, newCustomer);
 
